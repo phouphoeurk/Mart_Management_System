61d7efd067aaa0825ff44933e350ace95f4fd27b
itachi1920x1080 <itachi1920x1080@gmail.com>
2026-09-23 13:42:53 +0700

    Setup project structure and Database Connection

---
+++ Data/DatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace MartManagementSystem.Data
+{
+    public class DatabaseConnection
+    {
+        // កែប្រែ Server=ITACHI\SQLEXPRESS
+        private static readonly string connectionString = @"Server=ITACHI\SQLEXPRESS;Database=MartManagementDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Cannot connect to Database. Error: " + ex.Message);
+            }
+        }
+    }
+}
--- Forms/Form1.cs
+++ Forms/Form1.cs
@@ -1,3 +1,5 @@
+using MartManagementSystem.Data;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Windows.Forms;
 
@@ -9,6 +11,21 @@ namespace Mart_Management_System.Forms
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = DatabaseConnection.GetConnection())
+                {
+                    conn.Open();
+                    MessageBox.Show("ការតភ្ជាប់ Database ទទួលបានជោគជ័យ! (Connection Successful!)", "Database Setup", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ការតភ្ជាប់បរាជ័យ (Connection Failed): \n\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
         private void InitializeComponent()
         {
@@ -19,7 +36,13 @@ namespace Mart_Management_System.Forms
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Form1";
+
+            // បន្ថែមបន្ទាត់កូដនេះ ដើម្បីភ្ជាប់ Event ឱ្យដំណើរការកូដភ្ជាប់ Database
+            this.Load += new System.EventHandler(this.Form1_Load);
+
             this.ResumeLayout(false);
         }
+
+
     }
 }
