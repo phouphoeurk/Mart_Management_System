69ab249e1b20a677cdfe6a3167d0259bde191145
Phoeurk Phou <phophoeuk123@gmail.com>
2026-09-23 11:28:20 +0700

    Create a folder to hold all winForm file and repsoistory , model folder

--- obj/Debug/net8.0-windows/Mart_Management_System.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Mart_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mart_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd65cb94949058ab660c3d7b66b5a4d374f1f42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mart_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mart_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
