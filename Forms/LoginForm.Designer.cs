21432d7a6c5cb9d497e9f79eae078f340c21d434
Phoeurk Phou <phophoeuk123@gmail.com>
2026-09-23 17:12:35 +0700

    Add button

--- Forms/LoginForm.Designer.cs
+++ Forms/LoginForm.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LoginForm";
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(514, 335);
+            button1.Name = "button1";
+            button1.Size = new Size(371, 235);
+            button1.TabIndex = 0;
+            button1.Text = "Hello World";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1288, 819);
+            Controls.Add(button1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
