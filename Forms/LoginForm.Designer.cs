bcdbe35c037092575d774f839d6a5739826cdcef
Phoeurk Phou <phophoeuk123@gmail.com>
2026-09-23 11:55:22 +0700

    Design Login Form

--- Forms/LoginForm.Designer.cs
+++ Forms/LoginForm.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            btnCancel = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Snow;
+            label1.Location = new Point(185, 162);
+            label1.Name = "label1";
+            label1.Size = new Size(152, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ButtonFace;
+            label2.Location = new Point(185, 337);
+            label2.Name = "label2";
+            label2.Size = new Size(143, 41);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(185, 215);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(402, 47);
+            txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(185, 405);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(402, 47);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(128, 128, 255);
+            btnLogin.Location = new Point(399, 537);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(188, 58);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(170, 537);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(188, 58);
+            btnCancel.TabIndex = 5;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 192, 255);
+            ClientSize = new Size(782, 719);
+            Controls.Add(btnCancel);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Button btnCancel;
     }
 }
