dbd65cb94949058ab660c3d7b66b5a4d374f1f42
Phoeurk Phou <phophoeuk123@gmail.com>
2026-09-23 11:08:12 +0700

    Initial commit

---
+++ LoginForm.cs
@@ -0,0 +1,10 @@
+namespace Mart_Management_System
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
