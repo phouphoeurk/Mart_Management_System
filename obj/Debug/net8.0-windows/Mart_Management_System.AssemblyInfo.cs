a065bf0ed477ec491374f500a184e280e757abf7
itachi1920x1080 <itachi1920x1080@gmail.com>
2026-09-23 18:26:12 +0700

    Add DotNetEnv for database configuration

--- obj/Debug/net8.0-windows/Mart_Management_System.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Mart_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mart_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765084a92c7befb75a753369b307a62ed087f9bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781fc7db7b0ad475b220412acc712dba5258650e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mart_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mart_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
