769dd94cc0cb2f672ee7930c1ff5431b3a0d873e
Phoeurk Phou <phophoeuk123@gmail.com>
2026-09-25 10:28:24 +0700

    obj

--- obj/Debug/net8.0-windows/Mart_Management_System.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Mart_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mart_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0707034f16e245d072973d495827165db6d52347")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9434c2204b3d38f002a9b282b9e5b48ce9d89573")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mart_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mart_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
